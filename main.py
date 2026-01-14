627e7d2d35d89399fcbab72b8efb3e0994394b4c
SatriaAkbarRizki <sartiapl@gmail.com>
2026-01-14 11:47:25 +0700

    Update: increment security with give api key check

--- main.py
+++ main.py
@@ -4,15 +4,17 @@ import os
 from pathlib import Path
 from detecttext import TextOCRMobile
 from service import ServiceOperation
-from fastapi import FastAPI, File, UploadFile, HTTPException
+from dotenv import load_dotenv, dotenv_values
+from fastapi.security import APIKeyHeader
+from fastapi import Depends, FastAPI, File, UploadFile, HTTPException
 
 
 app = FastAPI()
+load_dotenv()
+header_scheme = APIKeyHeader(name="apiKey")
 
 logger = logging.getLogger("uvicorn.error")
 logger.propagate = False
-
-
 
 
 @app.get("/")
@@ -23,7 +25,9 @@ def root():
 
 
 @app.post("/files/uploadImage")
-def uploadImage(file: UploadFile | None = None):
+def uploadImage(file: UploadFile | None = None, key: str = Depends(header_scheme)):
+    if key != os.getenv('APIKEYOCR'):
+         raise HTTPException(status_code=401)
     if not file.content_type.startswith("image/"):
         raise HTTPException(status_code=400, detail="This not file image")
     else:
